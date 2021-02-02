3635f4e75969511af2bd79eeaafcdc0546011a6d
kuperavv <vince.kuperavage@gmail.com>
2021-02-01 19:34:51 -0600

    db

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,11 @@
 const { expect } = require('chai');
+const { syncAndSeed } = require('../db');
 
 const app = require('supertest')(require('../app'));
 
 describe('Test', () => {
+  beforeEach(() => syncAndSeed());
+
   describe('First Test', () => {
     it('passes a test', () => {
       expect(1).to.equal(1);
