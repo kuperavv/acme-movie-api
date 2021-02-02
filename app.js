f467a49c54ff33fce431b1a71fdd16e0292874e1
kuperavv <vince.kuperavage@gmail.com>
2021-02-01 19:28:54 -0600

    new test

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+  res.send('some text');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+module.exports = app;
--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,15 @@
 const { expect } = require('chai');
 
+const app = require('supertest')(require('../app'));
+
 describe('Test', () => {
   describe('First Test', () => {
     it('passes a test', () => {
       expect(1).to.equal(1);
+    });
+    it('pings the API', async () => {
+      const response = await app.get('/');
+      expect(response.status).to.equal(200);
     });
   });
 });
